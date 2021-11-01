d20c83e339e3d9dc44f38c46a217df60b6701c32
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-11-01 10:13:52 +0100

    Initial Commit

---
+++ MappeAppleTv/MappeAppleTvApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappeAppleTvApp.swift
+//  MappeAppleTv
+//
+//  Created by Michele Manniello on 01/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MappeAppleTvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
